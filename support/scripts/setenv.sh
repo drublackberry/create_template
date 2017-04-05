3d543b67b0fbb597d59e5067c4af2279b19cd96f
Andreu Mora <andreu.mora@gmail.com>
2017-04-05 11:34:38 +0200

    Changed script route to new location

--- support/scripts/setenv.sh
+++ support/scripts/setenv.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Call the script to set the environment
-python config/setenv.py
+python config/scripts/setenv.py
 
 # Set PYTHONPATH to point to the codebase
-NEW=`python config/get_env_src.py`
+NEW=`python config/scripts/get_env_src.py`
 export PYTHONPATH=$PYTHONPATH:$NEW
 
 # Set the PROJECT_ROOT
-PROJECT_DIR=`python config/get_env_dir.py`
+PROJECT_DIR=`python config/scripts/get_env_dir.py`
 echo $PROJECT_DIR
 export PROJECT_ROOT=$PROJECT_DIR
